7f55ff0eafe9f1ff75d00b53f7352363514c7743
Sayan Mondal <sanynmandal@gmail.com>
2026-03-14 10:08:23 +0530

    Add VisitorCounter component import to App

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import NotFound from './pages/NotFound.jsx'
 import SkillNetwork from './pages/Skills'
 import ThemeToggle from './pages/themeToggle'
 import SemesterTimeline from './pages/SemesterTimeline'
+import VisitorCounter from "./pages/VisitorCounter";
 
 export default function App() {
   return (
